f984a394a874721f4450e0595557d4a1a5931b0e
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-10-23 22:07:18 -0500

    Terminados los ejercicios 06-JS-V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -54,7 +54,7 @@ function agregarStringInvertida() {
   //Crea el constructor de la clase "Persona"
   //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
   //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
-  //Ej: { 
+  //Ej: {
     //   Nombre: 'Juan',
     //   Apellido: 'Perez',
     //   Edad: 22,
@@ -62,9 +62,21 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre, apellido, edad, domicilio) {
       // Crea el constructor:
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
 
+      this.detalle = function (){
+        return {
+          nombre : this.nombre,
+          apellido : this.apellido,
+          edad : this.edad,
+          domicilio : this.domicilio
+        }
+      }
     }
 }
 
@@ -72,13 +84,20 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+
+  var persona = new Persona('Juan', 'Perez', 22, 'Saavedra 123');
+
+  return persona;
 }
-  
+
 function agregarMetodo() {
-  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
+  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve:
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function () {
+    return `${this.nombre}, ${this.edad} años`;
+  }
 }
-  
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
